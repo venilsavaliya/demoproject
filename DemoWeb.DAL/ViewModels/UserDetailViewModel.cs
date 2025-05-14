b9fe5e9e1ed1b78b67b1a53cb7e27bf288f477e1
venil <venilsavaliya999@gmail.com>
2025-05-14 08:01:55 +0530

    add validation

--- DemoWeb.DAL/ViewModels/UserDetailViewModel.cs
+++ DemoWeb.DAL/ViewModels/UserDetailViewModel.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DemoWeb.DAL.ViewModels;
 
 public class UserDetailViewModel
 {
     public int Id { get; set; }
 
+    [Required]
     public string Name { get; set; } = null!;
 
     public string? Surname { get; set; }
